34d3a2bd7b27f133dbff7ad50cdcb77ef3442265
Homin Lee <ff4500@gmail.com>
2022-09-21 22:36:56 +0900

    add tag feature to push2gke.sh

--- push2gke.sh
+++ push2gke.sh
@@ -2,10 +2,10 @@
 git tag -a $1 -m "add tag for $1"
 git push --tags
 
-IMAGE_TAG=gcr.io/homin-dev/ingress:$1 
+IMAGE_TAG=gcr.io/homin-dev/ingress-proxy:$1 
 docker buildx build --platform linux/amd64 -t $IMAGE_TAG .
 docker push $IMAGE_TAG
 
-IMAGE_TAG_LATEST=gcr.io/homin-dev/ingress:latest 
-docker tag IMAGE_TAG IMAGE_TAG_LATEST
+IMAGE_TAG_LATEST=gcr.io/homin-dev/ingress-proxy:latest 
+docker tag $IMAGE_TAG $IMAGE_TAG_LATEST
 docker push $IMAGE_TAG_LATEST
