5e781258c45107da8f75a86ffb0eaff9cf26b04d
Homin Lee <ff4500@gmail.com>
2022-11-28 08:21:05 +0900

    add attrs to tracer

--- tracer.go
+++ tracer.go
@@ -4,12 +4,15 @@ import (
 	"context"
 
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/metric/global"
 	"go.opentelemetry.io/otel/propagation"
 	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -56,12 +59,12 @@ func initTracerProvider(ctx context.Context, url string) *sdktrace.TracerProvide
 	}
 	tp := sdktrace.NewTracerProvider(
 		sdktrace.WithBatcher(exporter),
-		// sdktrace.WithResource(resource.NewWithAttributes(
-		// 	semconv.SchemaURL,
-		// 	semconv.ServiceNameKey.String("homin-dev"),
-		// 	attribute.String("name", programName),
-		// 	attribute.String("ver", programVer),
-		// )),
+		sdktrace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String("homin-dev"),
+			attribute.String("name", programName),
+			attribute.String("ver", programVer),
+		)),
 	)
 	otel.SetTracerProvider(tp)
 	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
